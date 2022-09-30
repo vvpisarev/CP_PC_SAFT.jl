25593d079911884fa99f366c291a0fccc3907287
Stepan Zakharov <stepanzh@gmail.com>
2022-09-30 13:49:40 +0300

    testset structure

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,15 @@ using Test
 using CubicEoS
 using CP_PC_SAFT
 
-mixture = CubicEoS.load(CPPCSAFTMixture; names=("methane", "n-pentane"))
+@testset "CP_PC_SAFT.jl" begin
+
+c1c5 = CubicEoS.load(CPPCSAFTMixture; names=("methane", "n-pentane"))
 volume = 1e-6
 nmol = [0.6, 0.4] * 7000 * volume
-RT = CubicEoS.GAS_CONSTANT_SI * 500
+RT500 = CubicEoS.GAS_CONSTANT_SI * 500
 
-@test pressure(mixture, nmol, volume, RT) ≈ 2.8631703619799692e7
+@testset "basic_thermo" begin
+    @test pressure(c1c5, nmol, volume, RT500) ≈ 2.8631703619799692e7
+end
+
+end # @testset
