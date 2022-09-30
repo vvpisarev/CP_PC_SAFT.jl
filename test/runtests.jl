a2b3d7f96de38764483842e699adefb677d86154
Stepan Zakharov <stepanzh@gmail.com>
2022-09-30 14:03:57 +0300

    VT Stability tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,32 @@ using CP_PC_SAFT
 
 @testset "CP_PC_SAFT.jl" begin
 
-c1c5 = CubicEoS.load(CPPCSAFTMixture; names=("methane", "n-pentane"))
+mixture = CubicEoS.load(CPPCSAFTMixture; names=("methane", "n-pentane"))
 volume = 1e-6
 nmol = [0.6, 0.4] * 7000 * volume
-RT500 = CubicEoS.GAS_CONSTANT_SI * 500
+RT = CubicEoS.GAS_CONSTANT_SI * 500
 
 @testset "basic_thermo" begin
-    @test pressure(c1c5, nmol, volume, RT500) ≈ 2.8631703619799692e7
+    @test pressure(mixture, nmol, volume, RT) ≈ 2.8631703619799692e7
+end
+
+@testset "vt_stability" begin
+    @testset "Stable one phase state" begin
+        issuccess, isstable, trials = vt_stability(mixture, nmol, volume, RT;
+            tol=1e-6,
+            maxiter=1000,
+        )
+        @test issuccess && isstable
+    end
+
+    @testset "Unstable one phase state" begin
+        RTUnstable = CubicEoS.GAS_CONSTANT_SI * 300
+        issuccess, isstable, trials = vt_stability(mixture, nmol, volume, RTUnstable;
+            tol=1e-6,
+            maxiter=1000,
+        )
+        @test issuccess && !isstable
+    end
 end
 
 end # @testset
